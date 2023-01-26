2e7f1e0629921d2d4dc11e07f8d4aa282b146183
lucasx <l.kaminski@protonmail.com>
2023-01-26 19:02:04 +0100

    Setup app for deploy to gigalixir

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule SudokuSolver.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:benchee, "~> 1.0", only: :dev}
+      {:benchee, "~> 1.0", only: :dev},
+      {:distillery, "~> 2.1"}
     ]
   end
 
